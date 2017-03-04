a8497a271efd138fea94bf10174477e77134afe4
Deepak Kumar <Deepak.n.sonukumar@gmail.com>
2017-03-03 23:55:13 -0800

    Designing and Implementing Tables
    Designing and Implementing Tables Added!

---
+++ Developing SQL Databases/CreateCourier.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE SalesLT.Courier(
+CourierID int NOT NULL,
+CourierCode char(3) NOT NULL,
+CourierName nvarchar(50) NOT NULL,
+PRIMARY KEY (CourierID, CourierCode)
+);
+GO
+
+ALTER TABLE SalesLT.Courier
+ADD Telephone varchar(15) NULL, Email varchar(25) NULL;
+GO
---
+++ Developing SQL Databases/CreatePetStore.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE PetStore(
+PetID int IDENTITY(1,1) PRIMARY KEY,
+PetName nvarchar(30) NOT NULL,
+DateOfBirth date NOT NULL,
+YearOfBirth AS DATEPART(year, DateOfBirth) PERSISTED
+);
+GO
