cd6d864ca1e486dfff6c28e9e0f25e8edd700636
Deepak Kumar <Deepak.n.sonukumar@gmail.com>
2017-04-17 00:41:10 -0700

    Index Fragmentation
    Index Fragmentation Query Added!

---
+++ Developing SQL Databases/IndexFrag.sql
@@ -0,0 +1,4 @@
+﻿SELECT obj.name, phys.* 
+FROM sys.dm_db_index_physical_stats(DB_ID(),NULL,NULL,NULL,'LIMITED') as phys
+JOIN sys.objects as obj on obj.object_id = phys.object_id
+ORDER BY avg_fragmentation_in_percent DESC;
