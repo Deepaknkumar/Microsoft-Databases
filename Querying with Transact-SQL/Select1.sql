bad343f114a299f6c74efabfecf0947c8b9141c2
Deepak Kumar <Deepak.n.sonukumar@gmail.com>
2017-05-08 01:28:25 -0700

    Transact SQL
    Transact SQL Added!

---
+++ Querying with Transact-SQL/Select1.sql
@@ -0,0 +1,12 @@
+﻿SELECT ProductID, Name, Color, Size, Color+Size AS Style
+FROM SalesLT.Product;
+
+SELECT 'Hello World';
+
+SELECT * FROM SalesLT.Product;
+
+SELECT ProductID, Name , ListPrice, StandardCost
+FROM SalesLT.Product;
+
+SELECT ProductID, Name,ListPrice, StandardCost, ListPrice - StandardCost AS Margin
+FROM SalesLT.Product;
