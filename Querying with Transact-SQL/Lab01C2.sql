b848ee6acb841b5719d0a9ee1bc9dcc2d0c9a639
Deepak Kumar <Deepak.n.sonukumar@gmail.com>
2017-05-09 01:21:43 -0700

    Lab01 C2
    Lab01 C2 Added!

---
+++ Querying with Transact-SQL/Lab01C2.sql
@@ -0,0 +1,5 @@
+﻿SELECT SalesOrderNumber + ' (' + CAST(RevisionNumber AS VARCHAR(10)) + ')' AS OrderNumber, CONVERT(nvarchar(30),OrderDate) AS OrderDate
+FROM SalesLT.SalesOrderHeader;
+
+SELECT CAST(CustomerID AS varchar(10)) + ': ' + ISNULL(CompanyName,' N/A') AS CustomerCompanies
+FROM SalesLT.Customer;
