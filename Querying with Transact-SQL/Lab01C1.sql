9668722c37baa0bd6e1aa13cf4b27a2562293ac8
Deepak Kumar <Deepak.n.sonukumar@gmail.com>
2017-05-09 01:05:08 -0700

    Lab01
    Lab01 Added!

---
+++ Querying with Transact-SQL/Lab01C1.sql
@@ -0,0 +1,7 @@
+﻿SELECT SalesPerson, Title + ' ' + LastName AS CustomerName, Phone AS PhoneNumber
+FROM SalesLT.Customer;
+
+SELECT Title, FirstName, ISNULL(MiddleName, '') AS MiddleName, LastName, ISNULL(Suffix,'') AS Suffix 
+FROM SalesLT.Customer;
+
+SELECT * FROM SalesLT.Customer;
