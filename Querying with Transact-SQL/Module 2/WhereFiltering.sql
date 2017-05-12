1cc748d7c6e2cd1e5930b69a50146bc38e425aa3
Deepak Kumar <Deepak.n.sonukumar@gmail.com>
2017-05-12 00:31:08 -0700

    WHERE Clause
    WHERE Predicates Added!

---
+++ Querying with Transact-SQL/Module 2/WhereFiltering.sql
@@ -0,0 +1,31 @@
+﻿SELECT Name, Color, Size
+FROM SalesLT.Product 
+WHERE ProductModelID = 6;
+
+SELECT productnumber, Name, ListPrice
+FROM SalesLT.Product
+WHERE ProductNumber LIKE 'FR%';
+
+SELECT ProductNumber,Name, ListPrice
+FROM SalesLT.Product
+WHERE ProductNumber LIKE 'FR-_[0-9]__-[0-9][0-9]';
+
+SELECT Name
+FROM SalesLT.Product
+WHERE SellEndDate IS NOT NULL;
+
+SELECT Name
+FROM SalesLT.Product
+WHERE SellEndDate BETWEEN '1800/1/1' AND '2006/12/31';
+
+SELECT ProductCategoryID, Name, ListPrice
+FROM SalesLT.Product
+WHERE ProductCategoryID IN (5,6,7);
+
+SELECT ProductCategoryID, Name, ListPrice, SellEndDate
+FROM SalesLT.Product
+WHERE ProductCategoryID IN (5,6,7) AND SellEndDate IS NULL;
+
+SELECT Name, ProductCategoryID, ProductNumber
+FROM SalesLT.Product
+WHERE ProductNumber LIKE 'FR%' OR ProductCategoryID IN (5,6,7);
---
+++ Querying with Transact-SQL/Module 2/WherePredicates.sql
@@ -0,0 +1,7 @@
+﻿SELECT Name, Color, Size
+FROM SalesLT.Product 
+WHERE ProductModelID = 6;
+
+SELECT productnumber, Name, ListPrice
+FROM SalesLT.Product
+WHERE ProductNumber LIKE 'FR%'
