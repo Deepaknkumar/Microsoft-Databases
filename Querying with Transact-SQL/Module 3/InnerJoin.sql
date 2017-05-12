c15506633380c5aa489c36bf6f65676881ff1d79
Deepak Kumar <Deepak.n.sonukumar@gmail.com>
2017-05-12 01:46:29 -0700

    Inner Join Queries
    Inner Join Queries Added!

---
+++ Querying with Transact-SQL/Module 3/InnerJoin.sql
@@ -0,0 +1,29 @@
+﻿-- Basic inner join
+SELECT SalesLT.Product.Name AS ProductName, SalesLT.ProductCategory.Name AS Category
+FROM SalesLT.Product
+INNER JOIN SalesLT.ProductCategory
+ON SalesLT.Product.ProductCategoryID = SalesLT.ProductCategory.ProductCategoryID;
+
+-- Table Aliases
+SELECT p.Name AS ProductName, c.Name AS Category
+FROM SalesLT.Product AS p
+INNER JOIN SalesLT.ProductCategory AS c
+ON p.ProductCategoryID = c.ProductCategoryID;
+
+-- Joining more than 2 tables
+SELECT oh.OrderDate, oh.SalesOrderNumber, p.Name AS ProductName, od.OrderQty, od.UnitPrice, od.LineTotal
+FROM SalesLT.SalesOrderHeader AS oh
+INNER JOIN SalesLT.SalesOrderDetail AS od
+ON od.SalesOrderID = oh.SalesOrderID
+INNER JOIN SalesLT.Product AS p
+ON od.ProductID = p.ProductID
+ORDER BY oh.OrderDate, oh.SalesOrderID, od.SalesOrderDetailID;
+
+-- Multiple join parameters
+SELECT oh.OrderDate, oh.SalesOrderNumber, p.Name AS ProductName, od.OrderQty, od.UnitPrice
+FROM SalesLT.SalesOrderHeader AS oh
+INNER JOIN SalesLT.SalesOrderDetail AS od
+ON od.SalesOrderID = oh.SalesOrderID
+INNER JOIN SalesLT.Product AS p
+ON od.ProductID = p.ProductID AND od.UnitPrice < p.ListPrice
+ORDER BY oh.OrderDate, oh.SalesOrderID, od.SalesOrderDetailID;
