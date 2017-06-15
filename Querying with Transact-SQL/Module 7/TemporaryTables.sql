9311f42bdc534f0a279af2cb9545ea7ebf80029b
Deepak Kumar <Deepak.n.sonukumar@gmail.com>
2017-06-14 18:14:25 -0700

    Module 7
    Module 7 - Using Table Expressions Added!

---
+++ Querying with Transact-SQL/Module 7/TemporaryTables.sql
@@ -0,0 +1,22 @@
+﻿-- Temoporary Tables
+CREATE TABLE #Colors
+(COLOR VARCHAR(15));
+
+INSERT INTO #Colors
+SELECT DISTINCT Color FROM SalesLT.Product;
+
+SELECT * FROM #Colors;
+
+-- Table Variable
+DECLARE @Colors AS TABLE (Color VARCHAR(15));
+
+INSERT INTO @Colors
+SELECT DISTINCT Color FROM SalesLT.Product;
+
+SELECT * FROM @Colors;
+
+-- NEW Batch
+SELECT * FROM #Colors;
+
+SELECT * FROM @Colors; -- out of scope
+
---
+++ Querying with Transact-SQL/Module 7/ViewQuery.sql
@@ -0,0 +1,21 @@
+﻿-- Create a View
+CREATE VIEW SalesLT.vCustomerAddress
+AS
+SELECT C.CustomerID, FirstName, LastName, AddressLine1, City, StateProvince
+FROM SalesLT.Customer AS C
+JOIN SalesLT.CustomerAddress AS CA
+ON C.CustomerID = CA.CustomerID
+JOIN SalesLT.Address AS A
+ON CA.AddressID = A.AddressID;
+
+-- Query the view
+SELECT CustomerID, City
+FROM SalesLT.vCustomerAddress;
+
+-- Join the view to a table
+SELECT ca.StateProvince, ca.City, ISNULL(SUM(soh.TotalDue),0.00) AS Revenue
+FROM SalesLT.vCustomerAddress AS ca
+LEFT JOIN SalesLT.SalesOrderHeader AS soh
+ON ca.CustomerID = soh.CustomerID
+GROUP BY ca.StateProvince, ca.City
+ORDER BY ca.StateProvince, Revenue DESC;
