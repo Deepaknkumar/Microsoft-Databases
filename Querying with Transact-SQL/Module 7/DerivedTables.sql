c71e01c19f76743e5f9325aeeaded2a3a5a17510
Deepak Kumar <Deepak.n.sonukumar@gmail.com>
2017-06-15 23:06:21 -0700

    Derived Tables and Common Table Expressions
    Derived Tables and Common Table Expressions Added!

---
+++ Querying with Transact-SQL/Module 7/DerivedTables.sql
@@ -0,0 +1,8 @@
+﻿SELECT Category, COUNT(ProductID) AS Products
+FROM
+	(SELECT p.ProductID, p.Name, c.Name AS Category
+	FROM SalesLT.Product AS p
+	JOIN SalesLT.ProductCategory AS c
+	ON p.ProductCategoryID = c.ProductCategoryID) AS ProdCats
+	GROUP BY Category
+	ORDER BY Category;
