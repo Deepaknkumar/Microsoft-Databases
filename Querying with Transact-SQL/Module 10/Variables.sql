a92e82fb04f14b17e7c8aee6feb2898f7201b5f6
Deepak Kumar <Deepak.n.sonukumar@gmail.com>
2017-06-24 20:47:25 -0700

    Module 10
    Module 10 Added!

---
+++ Querying with Transact-SQL/Module 10/Variables.sql
@@ -0,0 +1,21 @@
+﻿-- Search by city using a variable
+DECLARE @City VARCHAR(20) = 'Toronto'
+--SET @City = 'Bellevue'
+--GO		-- Error
+
+SELECT FirstName + ' ' + LastName AS [NAME], AddressLine1 AS Address, City
+FROM SalesLT.Customer AS C
+JOIN SalesLT.CustomerAddress AS CA
+ON C.CustomerID = CA.CustomerID
+JOIN SalesLT.Address AS A
+ON A.AddressID = CA.AddressID
+WHERE City = @City;
+
+
+-- Use a variable as an output
+DECLARE @Result MONEY
+SELECT @Result = MAX(TotalDue)
+FROM SalesLT.SalesOrderHeader
+
+PRINT @Result;
+
---
+++ Querying with Transact-SQL/Module 10/WhileLoop.sql
@@ -0,0 +1,23 @@
+﻿-- Looping
+DECLARE @custid AS INT = 1, @lname AS NVARCHAR(20);
+WHILE @custid <= 5
+	BEGIN
+		SELECT @lname = lastname FROM SalesLT.Customer
+		WHERE CustomerID = @custid
+		PRINT @lname
+		SET @custid += 1
+	END
+
+-- While
+DECLARE @Counter INT = 1
+WHILE @Counter <= 5
+BEGIN
+	INSERT SalesLT.CallLog(SalesPerson,CustomerID,PhoneNumber,Notes)
+	VALUES ('Online Sale',7,'250-574-26424','ROW ' + CONVERT(VARCHAR(5),@Counter))
+	SET @Counter +=1
+	END
+
+SELECT Notes FROM SalesLT.CallLog;
+
+
+
